79500053adc5077e5da0c1cab340f2467720f8a2
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-10-16 00:27:01 -0600

    Add mid/side option to Stereo Splitter

---
+++ src/processors/utility/StereoSplitter.cpp
@@ -0,0 +1,86 @@
+#include "StereoSplitter.h"
+#include "../ParameterHelpers.h"
+
+StereoSplitter::StereoSplitter (UndoManager* um) : BaseProcessor ("Stereo Splitter", createParameterLayout(), um, 1, 2)
+{
+    modeParam = vts.getRawParameterValue ("mode");
+
+    uiOptions.backgroundColour = Colours::lightgrey;
+    uiOptions.powerColour = Colours::darkred.brighter (0.2f);
+    uiOptions.info.description = "Splits a stereo signal into two mono signals.";
+    uiOptions.info.authors = StringArray { "Jatin Chowdhury" };
+}
+
+AudioProcessorValueTreeState::ParameterLayout StereoSplitter::createParameterLayout()
+{
+    using namespace ParameterHelpers;
+    auto params = createBaseParams();
+
+    params.push_back (std::make_unique<AudioParameterChoice> ("mode",
+                                                              "Mode",
+                                                              StringArray { "Left/Right", "Mid/Side" },
+                                                              0));
+
+    return { params.begin(), params.end() };
+}
+
+void StereoSplitter::prepare (double sampleRate, int samplesPerBlock)
+{
+    ignoreUnused (sampleRate);
+
+    for (auto& buffer : buffers)
+        buffer.setSize (1, samplesPerBlock);
+}
+
+void StereoSplitter::processAudio (AudioBuffer<float>& buffer)
+{
+    const auto numChannels = buffer.getNumChannels();
+    const auto numSamples = buffer.getNumSamples();
+
+    bool splitLeftRight = *modeParam == 0.0f;
+
+    for (auto& b : buffers)
+        b.setSize (1, numSamples, false, false, true);
+
+    if (numChannels == 1)
+    {
+        if (splitLeftRight)
+        {
+            buffers[0].copyFrom (0, 0, buffer, 0, 0, numSamples);
+            buffers[1].copyFrom (0, 0, buffer, 0, 0, numSamples);
+        }
+        else
+        {
+            buffers[0].copyFrom (0, 0, buffer, 0, 0, numSamples);
+            buffers[1].clear (0, numSamples);
+        }
+    }
+    else if (numChannels == 2)
+    {
+        if (splitLeftRight)
+        {
+            buffers[0].copyFrom (0, 0, buffer, 0, 0, numSamples);
+            buffers[1].copyFrom (0, 0, buffer, 1, 0, numSamples);
+        }
+        else
+        {
+            buffers[0].copyFrom (0, 0, buffer, 1, 0, numSamples);
+            buffers[1].copyFrom (0, 0, buffer, 1, 0, numSamples);
+            buffers[1].applyGain (-1.0f);
+
+            buffers[0].addFrom (0, 0, buffer, 0, 0, numSamples);
+            buffers[1].addFrom (0, 0, buffer, 0, 0, numSamples);
+
+            constexpr float minus3dBGain = 1.0f / MathConstants<float>::sqrt2;
+            buffers[0].applyGain (minus3dBGain);
+            buffers[1].applyGain (minus3dBGain);
+        }
+    }
+    else
+    {
+        jassertfalse;
+    }
+
+    outputBuffers.getReference (0) = &buffers[0];
+    outputBuffers.getReference (1) = &buffers[1];
+}
--- src/processors/utility/StereoSplitter.h
+++ src/processors/utility/StereoSplitter.h
@@ -1,66 +1,22 @@
 #pragma once
 
 #include "../BaseProcessor.h"
-#include "../ParameterHelpers.h"
 
 class StereoSplitter : public BaseProcessor
 {
 public:
-    StereoSplitter (UndoManager* um) : BaseProcessor ("Stereo Splitter", createParameterLayout(), um, 1, 2)
-    {
-        uiOptions.backgroundColour = Colours::lightgrey;
-        uiOptions.powerColour = Colours::darkred.brighter (0.2f);
-        uiOptions.info.description = "Splits a stereo signal into two mono signals.";
-        uiOptions.info.authors = StringArray { "Jatin Chowdhury" };
-    }
+    StereoSplitter (UndoManager* um);
 
     ProcessorType getProcessorType() const override { return Utility; }
-    static AudioProcessorValueTreeState::ParameterLayout createParameterLayout()
-    {
-        using namespace ParameterHelpers;
-        auto params = createBaseParams();
+    static AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
 
-        return { params.begin(), params.end() };
-    }
-
-    void prepare (double sampleRate, int samplesPerBlock) override
-    {
-        ignoreUnused (sampleRate);
-
-        leftBuffer.setSize (1, samplesPerBlock);
-        rightBuffer.setSize (1, samplesPerBlock);
-    }
-
-    void processAudio (AudioBuffer<float>& buffer) override
-    {
-        const auto numChannels = buffer.getNumChannels();
-        const auto numSamples = buffer.getNumSamples();
-
-        leftBuffer.setSize (1, numSamples, false, false, true);
-        rightBuffer.setSize (1, numSamples, false, false, true);
-
-        if (numChannels == 1)
-        {
-            leftBuffer.copyFrom (0, 0, buffer, 0, 0, numSamples);
-            rightBuffer.copyFrom (0, 0, buffer, 0, 0, numSamples);
-        }
-        else if (numChannels == 2)
-        {
-            leftBuffer.copyFrom (0, 0, buffer, 0, 0, numSamples);
-            rightBuffer.copyFrom (0, 0, buffer, 1, 0, numSamples);
-        }
-        else
-        {
-            jassertfalse;
-        }
-
-        outputBuffers.getReference (0) = &leftBuffer;
-        outputBuffers.getReference (1) = &rightBuffer;
-    }
+    void prepare (double sampleRate, int samplesPerBlock) override;
+    void processAudio (AudioBuffer<float>& buffer) override;
 
 private:
-    AudioBuffer<float> leftBuffer;
-    AudioBuffer<float> rightBuffer;
+    std::atomic<float>* modeParam = nullptr;
+
+    AudioBuffer<float> buffers[2];
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (StereoSplitter)
 };
