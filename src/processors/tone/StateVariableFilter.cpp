2c1605f2b1cae3b0a0045fc938227937f56e5860
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-12-05 17:17:19 +0000

    Fix parameter smoothing for SVF

--- src/processors/tone/StateVariableFilter.cpp
+++ src/processors/tone/StateVariableFilter.cpp
@@ -41,27 +41,66 @@ AudioProcessorValueTreeState::ParameterLayout StateVariableFilter::createParamet
 void StateVariableFilter::prepare (double sampleRate, int samplesPerBlock)
 {
     svf.prepare ({ sampleRate, (uint32) samplesPerBlock, 2 });
+
+    freqSmooth.reset (sampleRate, 0.01);
+    freqSmooth.setCurrentAndTargetValue (*freqParam);
+}
+
+template <chowdsp::StateVariableFilterType type>
+void processFilter (AudioBuffer<float>& buffer, chowdsp::StateVariableFilter<float>& svf, StateVariableFilter::FreqSmooth& freqSmooth, float freqParam)
+{
+    const auto numChannels = buffer.getNumChannels();
+    const auto numSamples = buffer.getNumSamples();
+
+    freqSmooth.setTargetValue (freqParam);
+    if (freqSmooth.isSmoothing())
+    {
+        if (numChannels == 1)
+        {
+            auto* x = buffer.getWritePointer (0);
+            for (int n = 0; n < numSamples; ++n)
+            {
+                svf.setCutoffFrequency (freqSmooth.getNextValue());
+                x[n] = svf.template processSample<type> (0, x[n]);
+            }
+        }
+        else if (numChannels == 2)
+        {
+            auto* left = buffer.getWritePointer (0);
+            auto* right = buffer.getWritePointer (1);
+            for (int n = 0; n < numSamples; ++n)
+            {
+                svf.setCutoffFrequency (freqSmooth.getNextValue());
+                left[n] = svf.template processSample<type> (0, left[n]);
+                right[n] = svf.template processSample<type> (1, right[n]);
+            }
+        }
+    }
+    else
+    {
+        auto block = dsp::AudioBlock<float> { buffer };
+        auto context = dsp::ProcessContextReplacing<float> { block };
+        svf.process<decltype (context), type> (context);
+    }
 }
 
 void StateVariableFilter::processAudio (AudioBuffer<float>& buffer)
 {
-    svf.setCutoffFrequency (*freqParam);
     svf.setResonance (*qParam);
-
-    auto block = dsp::AudioBlock<float> { buffer };
-    auto context = dsp::ProcessContextReplacing<float> { block };
 
     auto mode = (int) *modeParam;
     switch (mode)
     {
         case 0:
-            svf.process<decltype (context), chowdsp::StateVariableFilterType::Lowpass> (context);
+            processFilter<chowdsp::StateVariableFilterType::Lowpass> (buffer, svf, freqSmooth, *freqParam);
             break;
         case 1:
-            svf.process<decltype (context), chowdsp::StateVariableFilterType::Highpass> (context);
+            processFilter<chowdsp::StateVariableFilterType::Highpass> (buffer, svf, freqSmooth, *freqParam);
             break;
         case 2:
-            svf.process<decltype (context), chowdsp::StateVariableFilterType::Bandpass> (context);
+            processFilter<chowdsp::StateVariableFilterType::Bandpass> (buffer, svf, freqSmooth, *freqParam);
+            break;
+        default:
             break;
     }
 }
