7f71e290621c12f26e8f089454dc0054d903d001
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-01-21 00:22:43 +0000

    Small improvements to chorus effect

--- src/processors/other/Chorus.h
+++ src/processors/other/Chorus.h
@@ -14,11 +14,17 @@ public:
     void prepare (double sampleRate, int samplesPerBlock) override;
     void processAudio (AudioBuffer<float>& buffer) override;
 
+    void addToPopupMenu (PopupMenu& menu) override;
+
 private:
+    template <typename DelayArrType>
+    void processChorus (AudioBuffer<float>& buffer, DelayArrType& delay);
+
     std::atomic<float>* rateParam = nullptr;
     std::atomic<float>* depthParam = nullptr;
     std::atomic<float>* fbParam = nullptr;
     std::atomic<float>* mixParam = nullptr;
+    std::atomic<float>* delayTypeParam = nullptr;
 
     dsp::DryWetMixer<float> dryWetMixer;
 
@@ -30,8 +36,37 @@ private:
     chowdsp::SineWave<float> slowLFOs[2][delaysPerChannel];
     chowdsp::SineWave<float> fastLFOs[2][delaysPerChannel];
 
-    using DelayType = chowdsp::BBD::BBDDelayWrapper<1024>;
-    std::array<std::array<DelayType, delaysPerChannel>, 2> delay;
+    struct CleanDelayType
+    {
+        void prepare (dsp::ProcessSpec& spec)
+        {
+            lpf.prepare (spec);
+            delay.prepare (spec);
+        }
+
+        void reset()
+        {
+            lpf.reset();
+            delay.reset();
+        }
+
+        void setDelay (float newDelayInSamples) { delay.setDelay (newDelayInSamples); }
+        void setFilterFreq (float freqHz) { lpf.setCutoffFrequency (freqHz); }
+
+        inline void pushSample (int channel, float sample) { delay.pushSample (channel, sample); }
+        inline float popSample (int channel) { return lpf.processSample<chowdsp::StateVariableFilterType::Lowpass> (channel, delay.popSample (channel)); }
+
+        chowdsp::StateVariableFilter<float> lpf;
+        chowdsp::DelayLine<float, chowdsp::DelayLineInterpolationTypes::Lagrange5th> delay { 1 << 18 };
+    };
+
+    template <typename DelayType>
+    using DelaySet = std::array<std::array<DelayType, delaysPerChannel>, 2>;
+    DelaySet<CleanDelayType> cleanDelay;
+
+    using LofiDelayType = chowdsp::BBD::BBDDelayWrapper<1024>;
+    DelaySet<LofiDelayType> lofiDelay;
+    int prevDelayTypeIndex = 0;
 
     chowdsp::StateVariableFilter<float> aaFilter;
 
@@ -41,6 +76,8 @@ private:
 
     float fs = 48000.0f;
     AudioBuffer<float> stereoBuffer;
+
+    std::unique_ptr<ParameterAttachment> delayTypeAttach;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Chorus)
 };
