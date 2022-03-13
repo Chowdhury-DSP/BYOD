2504150168734c6fb66b04d52ff035eec7c761fb
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-03-13 21:11:57 +0000

    Print diagnostics to log files when starting plugin

--- src/gui/utils/SettingsButton.cpp
+++ src/gui/utils/SettingsButton.cpp
@@ -1,4 +1,5 @@
 #include "SettingsButton.h"
+#include "SystemInfo.h"
 #include "gui/BoardViewport.h"
 #include "processors/chain/ProcessorChainPortMagnitudesHelper.h"
 
@@ -140,30 +141,5 @@ void SettingsButton::openGLManu (PopupMenu& menu, int itemID)
 void SettingsButton::copyDiagnosticInfo()
 {
     Logger::writeToLog ("Copying diagnostic info...");
-
-    String diagString;
-    diagString += "Version: " + proc.getName() + " " + String (JucePlugin_VersionString) + "\n";
-    diagString += "Commit: " + String (BYOD_GIT_COMMIT_HASH) + " on " + String (BYOD_GIT_BRANCH)
-                  + " with JUCE version " + SystemStats::getJUCEVersion() + "\n";
-
-    // build system info
-    diagString += "Build: " + Time::getCompilationDate().toString (true, true, false, true)
-                  + " on " + String (BYOD_BUILD_FQDN)
-                  + " with " + String (BYOD_CXX_COMPILER_ID) + "-" + String (BYOD_CXX_COMPILER_VERSION) + "\n";
-
-    // user system info
-    diagString += "System: " + SystemStats::getDeviceDescription()
-                  + " with " + SystemStats::getOperatingSystemName()
-                  + (SystemStats::isOperatingSystem64Bit() ? " (64-bit)" : String())
-                  + (SystemStats::isRunningInAppExtensionSandbox() ? " (Sandboxed)" : String())
-                  + " on " + String (SystemStats::getNumCpus()) + " Core, " + SystemStats::getCpuModel() + "\n";
-
-    // plugin info
-    PluginHostType hostType {};
-    diagString += "Plugin Info: " + String (proc.getWrapperTypeString())
-                  + " running in " + String (hostType.getHostDescription())
-                  + " running at " + String (proc.getSampleRate() / 1000.0, 1) + " kHz "
-                  + "with block size: " + String (proc.getBlockSize()) + "\n";
-
-    SystemClipboard::copyTextToClipboard (diagString);
+    SystemClipboard::copyTextToClipboard (SystemInfo::getDiagnosticsString (proc));
 }
