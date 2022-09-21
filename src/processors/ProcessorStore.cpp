59327511c2205bf5cf0d3f8951d94e39dfb429a6
Kai Mikkelsen <squabee@gmail.com>
2022-09-20 17:11:23 -0700

    rebase formatting

--- src/processors/ProcessorStore.cpp
+++ src/processors/ProcessorStore.cpp
@@ -53,7 +53,6 @@
 #if BYOD_ENABLE_ADD_ON_MODULES
 #include <AddOnProcessors.h>
 #include <AddOnProcessorStore.h>
-
 #endif
 
 template <typename ProcType>
@@ -130,6 +129,7 @@ ProcessorStore::ProcessorStore (UndoManager* um) : undoManager (um)
                                                {
                                                    auto proc = procFactory (undoManager);
                                                    jassert (name == proc->getName());
+                                                   
                                                    return std::make_pair (name, ProcInfo { proc->getProcessorType(), proc->getNumInputs(), proc->getNumOutputs() }); }));
     }
 
