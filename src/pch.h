63cd30b48a678c3da70aa743cbf7d611771eb463
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-10-08 15:38:21 -0700

    Fix Xcode warning in pch

--- src/pch.h
+++ src/pch.h
@@ -17,7 +17,9 @@
 #include <RTNeural/RTNeural.h>
 #include <chowdsp_wdf/chowdsp_wdf.h>
 #include <magic_enum.hpp>
+JUCE_BEGIN_IGNORE_WARNINGS_GCC_LIKE("-Wunused-parameter")
 #include <rapidfuzz/fuzz.hpp>
+JUCE_END_IGNORE_WARNINGS_GCC_LIKE
 #include <sst/cpputils.h>
 
 // global definitions
