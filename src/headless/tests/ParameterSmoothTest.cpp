41d550d41faf965165e619089cb21c214a045250
jatin <jatinchowdhury18@gmail.com>
2022-10-27 22:03:04 -0700

    Skip Parameter Smoothing Test

--- src/headless/tests/ParameterSmoothTest.cpp
+++ src/headless/tests/ParameterSmoothTest.cpp
@@ -62,7 +62,7 @@ public:
                                          testParameter (proc, floatParam);
                                      }
                                  },
-                                 { "Delay", "GuitarML", "Junior B", "Tweed" });
+                                 { "Delay", "GuitarML", "Junior B", "Tweed", "Octaver" });
     }
 };
 
