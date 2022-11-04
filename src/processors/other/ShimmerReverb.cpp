682026bca5d28407039246b1438c1a85aaea4bfe
jatin <jatinchowdhury18@gmail.com>
2022-11-03 17:45:55 -0700

    Add shimmer reverb effect

---
+++ src/processors/other/ShimmerReverb.cpp
@@ -0,0 +1,175 @@
+#include "ShimmerReverb.h"
+#include "processors/ParameterHelpers.h"
+
+namespace
+{
+const String shiftTag = "shift";
+const String sizeTag = "size";
+const String feedbackTag = "feedback";
+const String mixTag = "mix";
+} // namespace
+
+void ShimmerReverb::ShimmerFDNConfig::prepare (double sampleRate)
+{
+    shifter.prepare ({ sampleRate, 128, 2 });
+
+    highCutFilter.prepare (numFDNChannels);
+    highCutFilter.calcCoefs (10000.0f, (float) sampleRate);
+
+    lowCutFilter.prepare (numFDNChannels);
+    lowCutFilter.calcCoefs (55.0f, (float) sampleRate);
+
+    Base::prepare (sampleRate);
+}
+
+void ShimmerReverb::ShimmerFDNConfig::reset()
+{
+    shifter.reset();
+    highCutFilter.reset();
+    lowCutFilter.reset();
+    Base::reset();
+}
+
+const float* ShimmerReverb::ShimmerFDNConfig::doFeedbackProcess (ShimmerFDNConfig& fdnConfig, const float* data)
+{
+    auto filterChannel = [&fdnConfig] (float x, size_t channel)
+    {
+        x = fdnConfig.lowCutFilter.processSample (x, (int) channel);
+        x = fdnConfig.highCutFilter.processSample (x, (int) channel);
+        return x;
+    };
+
+    auto* fbData = fdnConfig.fbData.data();
+
+    const auto shiftIndex = (size_t) numFDNChannels - 1;
+    fbData[shiftIndex] = filterChannel (fdnConfig.shifter.processSample (0, data[shiftIndex]), shiftIndex);
+
+    for (size_t i = 0; i < shiftIndex; ++i)
+        fbData[i] = filterChannel (data[i], i);
+
+    return Base::doFeedbackProcess (fdnConfig, fbData);
+}
+
+//==================================================
+ShimmerReverb::ShimmerReverb (UndoManager* um) : BaseProcessor ("Shimmer Reverb", createParameterLayout(), um)
+{
+    using namespace ParameterHelpers;
+    shiftParam.setParameterHandle (getParameterPointer<chowdsp::FloatParameter*> (vts, shiftTag));
+    sizeParam.setParameterHandle (getParameterPointer<chowdsp::FloatParameter*> (vts, sizeTag));
+    feedbackParam.setParameterHandle (getParameterPointer<chowdsp::FloatParameter*> (vts, feedbackTag));
+    loadParameterPointer (mixParam, vts, mixTag);
+
+    uiOptions.backgroundColour = Colours::lightgrey;
+    uiOptions.powerColour = Colours::darksalmon.darker (0.2f);
+    uiOptions.info.description = "A pitch-shifting reverb effect.";
+    uiOptions.info.authors = StringArray { "Jatin Chowdhury" };
+}
+
+ParamLayout ShimmerReverb::createParameterLayout()
+{
+    using namespace ParameterHelpers;
+    auto params = createBaseParams();
+
+    emplace_param<chowdsp::FloatParameter> (params,
+                                            shiftTag,
+                                            "Shift",
+                                            NormalisableRange { -12.0f, 12.0f },
+                                            -12.0f,
+                                            &floatValToString,
+                                            &stringToFloatVal);
+    createPercentParameter (params, sizeTag, "Size", 1.0f);
+    createPercentParameter (params, feedbackTag, "Decay", 1.0f);
+    createPercentParameter (params, mixTag, "Mix", 0.5f);
+
+    return { params.begin(), params.end() };
+}
+
+void ShimmerReverb::prepare (double sampleRate, int samplesPerBlock)
+{
+    shiftParam.prepare (sampleRate, samplesPerBlock);
+    shiftParam.setRampLength (0.05);
+    sizeParam.prepare (sampleRate, samplesPerBlock);
+    sizeParam.setRampLength (0.2);
+    feedbackParam.prepare (sampleRate, samplesPerBlock);
+    feedbackParam.setRampLength (0.05);
+
+    sizeParam.mappingFunction = [] (float x)
+    {
+        const auto delayMs = 50.0f * std::pow (250.0f / 50.0f, x);
+        return delayMs;
+    };
+
+    feedbackParam.mappingFunction = [] (float x)
+    {
+        const auto delayMs = 1000.0f * std::pow (10000.0f / 1000.0f, x);
+        return delayMs;
+    };
+
+    for (auto& channelFDN : fdn)
+        channelFDN.prepare (sampleRate);
+
+    mixer.prepare ({ sampleRate, (uint32_t) samplesPerBlock, 2 });
+    mixer.setMixingRule (dsp::DryWetMixingRule::sin3dB);
+
+    for (int i = 0; i < numLFOs; ++i)
+    {
+        lfos[i].prepare ({ sampleRate, (uint32_t) samplesPerBlock, 1 });
+        lfoVals[i] = 0;
+    }
+}
+
+void ShimmerReverb::processAudio (AudioBuffer<float>& buffer)
+{
+    const auto numChannels = buffer.getNumChannels();
+    const auto numSamples = buffer.getNumSamples();
+
+    shiftParam.process (numSamples);
+    sizeParam.process (numSamples);
+    feedbackParam.process (numSamples);
+
+    mixer.pushDrySamples (dsp::AudioBlock<float> { buffer });
+    mixer.setWetMixProportion (mixParam->getCurrentValue());
+
+    static constexpr int smallBlockSize = 32;
+    auto* x = buffer.getArrayOfWritePointers();
+    const auto* shiftData = shiftParam.getSmoothedBuffer();
+    const auto* sizeData = sizeParam.getSmoothedBuffer();
+    const auto* feedbackData = feedbackParam.getSmoothedBuffer();
+
+    for (int i = 0; i < numSamples;)
+    {
+        const auto smallBlockSamples = jmin (smallBlockSize, numSamples - i);
+
+        for (float& lfoVal : lfoVals)
+            lfoVal = lfoVal * 0.1f + 1.0f;
+
+        for (int ch = 0; ch < numChannels; ++ch)
+        {
+            fdn[ch].setDelayTimeMsWithModulators<numLFOs> (sizeData[i], lfoVals);
+            fdn[ch].getFDNConfig().setDecayTimeMs (fdn[ch], feedbackData[i], feedbackData[i] * 0.75f, 800.0f);
+            fdn[ch].getFDNConfig().shifter.setShiftSemitones (shiftData[i]);
+
+            for (int n = i; n < i + smallBlockSamples; ++n)
+            {
+                const auto input = x[ch][n];
+
+                alignas (chowdsp::SIMDUtils::defaultSIMDAlignment) float fdnIn[numFDNChannels] {};
+                std::fill (std::begin (fdnIn), std::end (fdnIn), input);
+                const auto* fdnOut = fdn[ch].process (fdnIn);
+                const auto output = std::accumulate (fdnOut, fdnOut + numFDNChannels, 0.0f) / (float) numFDNChannels;
+
+                x[ch][n] = output;
+            }
+        }
+
+        for (int j = 0; j < smallBlockSamples; ++j)
+        {
+            for (int k = 0; k < numLFOs; ++k)
+                lfoVals[k] = lfos[k].processSample();
+        }
+
+        i += smallBlockSamples;
+    }
+
+    mixer.mixWetSamples (dsp::AudioBlock<float> { buffer });
+}
---
+++ src/processors/other/ShimmerReverb.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "processors/BaseProcessor.h"
+
+class ShimmerReverb : public BaseProcessor
+{
+public:
+    explicit ShimmerReverb (UndoManager* um);
+
+    ProcessorType getProcessorType() const override { return Other; }
+    static ParamLayout createParameterLayout();
+
+    void prepare (double sampleRate, int samplesPerBlock) override;
+    void processAudio (AudioBuffer<float>& buffer) override;
+
+private:
+    chowdsp::SmoothedBufferValue<float> shiftParam;
+    chowdsp::SmoothedBufferValue<float, ValueSmoothingTypes::Multiplicative> sizeParam;
+    chowdsp::SmoothedBufferValue<float, ValueSmoothingTypes::Multiplicative> feedbackParam;
+    chowdsp::FloatParameter* mixParam = nullptr;
+
+    static constexpr int numFDNChannels = 12;
+    struct ShimmerFDNConfig : chowdsp::Reverb::DefaultFDNConfig<float, numFDNChannels>
+    {
+        using Base = chowdsp::Reverb::DefaultFDNConfig<float, numFDNChannels>;
+        void prepare (double sampleRate) override;
+        void reset() override;
+        static const float* doFeedbackProcess (ShimmerFDNConfig& fdnConfig, const float* data);
+
+        chowdsp::PitchShifter<float, chowdsp::DelayLineInterpolationTypes::Sinc<float, 16, 2048>> shifter;
+        chowdsp::FirstOrderHPF<float> lowCutFilter;
+        chowdsp::FirstOrderLPF<float> highCutFilter;
+    };
+
+    chowdsp::Reverb::FDN<ShimmerFDNConfig> fdn[2];
+
+    static constexpr int numLFOs = 2;
+    chowdsp::SineWave<float> lfos[numLFOs];
+    float lfoVals[numLFOs] {};
+
+    dsp::DryWetMixer<float> mixer;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ShimmerReverb)
+};
