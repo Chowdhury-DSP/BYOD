389070b00bfb38025da9b3a71f31a5337dbb0051
Butch Warns <contact@butchwarns.de>
2023-04-03 17:15:49 +0200

    Add one-pole TPT filters

---
+++ src/processors/tone/ladder_filter/HighPassOnePole.h
@@ -0,0 +1,42 @@
+#pragma once
+
+// One-pole high-pass filter modeled by the TPT approach.
+// Used in the transistor-ladder filter cascade.
+// (See Zavalishin "The Art of VA Filter Design")
+class HighPassOnePole
+{
+public:
+    HighPassOnePole() = default;
+    ~HighPassOnePole() = default;
+
+    // Initialize filter
+    void reset (const double _sample_rate);
+
+    double get_sample_rate() const;
+
+    //==============================================================================
+
+    // Set the "instantaneous gain" of the filter
+    // Indirectly sets the filter cutoff frequency
+    // (G is calculated when setting the cutoff of the full ladder structure)
+    void set_G (const double _G);
+
+    // Set the "instantaneous gain" of the trapezoidal integrator times two
+    // (g2 is calculated when setting the cutoff of the full ladder structure)
+    void set_g2 (const double _g2);
+
+    // Get current contents of the internal state register
+    double get_state() const;
+
+    //==============================================================================
+
+    // Process a single audio sample
+    double process (double x);
+
+private:
+    double sample_rate { 0.0 };
+    double state { 0.0 }; // State register
+
+    double G { 0.0 }; // Filter "instantaneous gain"
+    double g2 { 0.0 }; // TPT integrator "instantaneous gain" times two
+};
---
+++ src/processors/tone/ladder_filter/LowPassOnePole.h
@@ -0,0 +1,37 @@
+#pragma once
+
+// One-pole low-pass filter modeled by the TPT approach.
+// Used in the transistor-ladder filter cascade.
+// (See Zavalishin "The Art of VA Filter Design")
+class LowPassOnePole
+{
+public:
+    LowPassOnePole() = default;
+    ~LowPassOnePole() = default;
+
+    // Initialize filter
+    void reset (const double _sample_rate);
+
+    double get_sample_rate() const;
+
+    //==============================================================================
+
+    // Set "instantaneous gain" of the filter
+    // Indirectly sets the filter cutoff frequency
+    // (G is calculated when setting the cutoff of the full ladder structure)
+    void set_G (const double _g);
+
+    // Get current contents of the internal state register
+    double get_state() const;
+
+    //==============================================================================
+
+    // Process a single audio sample
+    double process (double x);
+
+private:
+    double sample_rate { 0.0 };
+    double state { 0.0 }; // Internal state register
+
+    double G { 0.0 }; // Filter "instantaneous gain"
+};
