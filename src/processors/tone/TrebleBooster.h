a7aab7ebfd6d20e923331e05ddf0612b30009bb3
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2023-05-22 18:27:31 -0700

    Add netlist viewers for Baxandall EQ and Treble Booster (#286)
    * Add ntlist viewer for Baxandall EQ
    
    * Add netlist viewer for Treble Booster

--- src/processors/tone/TrebleBooster.h
+++ src/processors/tone/TrebleBooster.h
@@ -16,32 +16,42 @@ public:
 private:
     inline void calcCoefs (float curTreble) noexcept
     {
-        const float G2 = 1.0f / (1.8e3f + (1.0f - curTreble) * Rpot);
-        const float G3 = 1.0f / (4.7e3f + curTreble * Rpot);
+        const float G2 = 1.0f / (R21 + (1.0f - curTreble) * Rpot);
+        const float G3 = 1.0f / (R23 + curTreble * Rpot);
 
-        constexpr float wc = G1 / C; // frequency to match
+        float wc = G1 / C; // frequency to match
         const auto K = wc / std::tan (wc / (2.0f * fs)); // frequency warp to match transition freq
 
         // analog coefficients
-        float b_s[] { C * (G1 + G2), G1 * (G2 + G3) };
-        float a_s[] { C * (G3 - G4), -G4 * (G2 + G3) };
+        const float b_s[] { C * (G1 + G2), G1 * (G2 + G3) };
+        const float a_s[] { C * (G3 - G4), -G4 * (G2 + G3) };
+        const auto root = -a_s[1] / a_s[0];
 
         // bilinear transform
         float aU[2], bU[2];
         chowdsp::ConformalMaps::Transform<float, 1>::bilinear (bU, aU, b_s, a_s, K);
 
-        // flip pole inside unit circle to ensure stability
-        float b[] { bU[0] / aU[1], bU[1] / aU[1] };
-        float a[] { 1.0f, 1.0f / aU[1] };
-
-        for (auto& filt : iir)
-            filt.setCoefs (b, a);
+        if (root > 0.0f)
+        {
+            // flip pole inside unit circle to ensure stability
+            float b[] { bU[0] / aU[1], bU[1] / aU[1] };
+            float a[] { 1.0f, 1.0f / aU[1] };
+            for (auto& filt : iir)
+                filt.setCoefs (b, a);
+        }
+        else
+        {
+            for (auto& filt : iir)
+                filt.setCoefs (bU, aU);
+        }
     }
 
-    static constexpr float Rpot = 10e3f;
-    static constexpr float C = 3.9e-9f;
-    static constexpr float G1 = 1.0f / 100e3f;
-    static constexpr float G4 = 1.0f / 100e3f;
+    float Rpot = 10e3f;
+    float C = 3.9e-9f;
+    float G1 = 1.0f / 100e3f; // R22
+    float G4 = 1.0f / 100e3f; // R24
+    float R21 = 1.8e3f;
+    float R23 = 4.7e3f;
 
     chowdsp::FloatParameter* trebleParam = nullptr;
 
