a0c43265f1de59323144590a31d4a9b3fcf51031
jatin <jatinchowdhury18@gmail.com>
2023-07-06 19:04:21 -0700

    Trying to fix MSVC linker error

--- src/BYOD.cpp
+++ src/BYOD.cpp
@@ -136,7 +136,7 @@ static const clap_preset_discovery_factory byod_preset_discovery_factory {
     .create = preset_discovery::create,
 };
 
-const void* clapJuceExtensionCustomFactory (const char* factory_id)
+const void* JUCE_CALLTYPE clapJuceExtensionCustomFactory (const char* factory_id)
 {
     if (strcmp (factory_id, CLAP_PRESET_DISCOVERY_FACTORY_ID) == 0)
     {
